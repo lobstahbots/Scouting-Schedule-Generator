ae62f2a9f05a02af9ac212696c516314396a2ad9
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2024-04-16 17:36:29 -0400

    Added prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.ts",
-  devtool: "inline-source-map",
-  mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
-    libraryTarget: "umd",
-    library: "scouting-schedule-generator",
-    globalObject: "this",
-  },
+    entry: "src/index.ts",
+    devtool: "inline-source-map",
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: require.resolve("ts-loader"),
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "dist"),
+        libraryTarget: "umd",
+        library: "scouting-schedule-generator",
+        globalObject: "this",
+    },
 };
