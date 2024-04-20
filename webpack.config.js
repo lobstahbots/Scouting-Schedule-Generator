e07459cf328b7b78e3772ea7879c912b0a26b394
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2024-04-19 21:39:42 -0400

    Also compile for ESM and web

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,91 @@
-const path = require("path");
+import exp from "constants";
+import path from "path";
+import { fileURLToPath } from "url";
 
-module.exports = {
-    entry: "./src/index.ts",
-    devtool: "inline-source-map",
-    mode: "production",
-    entry: {
-        index: "./src/index.ts",
-        cli: "./src/cli.ts",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: require.resolve("ts-loader"),
-                exclude: /node_modules/,
+export default [
+    {
+        devtool: "inline-source-map",
+        mode: "production",
+        entry: {
+            index: "./src/index.ts",
+            cli: "./src/cli.ts",
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".ts", ".js"],
+        },
+        output: {
+            filename: "[name].cjs",
+            path: path.resolve(path.dirname(fileURLToPath(import.meta.url)), "dist"),
+            library: {
+                type: "umd",
+                name: "ScoutingScheduleGenerator",
             },
-        ],
+            globalObject: "this",
+            chunkFormat: "commonjs",
+        },
+        target: "node",
     },
-    resolve: {
-        extensions: [".ts", ".js"],
+    {
+        entry: "./src/index.ts",
+        experiments: {
+            outputModule: true,
+        },
+        devtool: "inline-source-map",
+        mode: "production",
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".ts", ".js"],
+        },
+        output: {
+            filename: "index.js",
+            path: path.resolve(path.dirname(fileURLToPath(import.meta.url)), "dist"),
+            library: {
+                type: "module",
+            },
+            globalObject: "this",
+            chunkFormat: "module",
+        },
+        target: "node",
     },
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "dist"),
-        libraryTarget: "umd",
-        library: "scouting-schedule-generator",
-        globalObject: "this",
+    {
+        entry: "./src/index.ts",
+        mode: "production",
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".ts", ".js"],
+        },
+        output: {
+            filename: "index.web.js",
+            path: path.resolve(path.dirname(fileURLToPath(import.meta.url)), "dist"),
+            library: { name: "ScoutingScheduleGenerator", type: "umd" },
+            globalObject: "this",
+            chunkFormat: "array-push",
+        },
+        target: "web",
     },
-    target: "node",
-};
+];
